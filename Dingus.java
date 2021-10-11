aaae9df7579a3e3fa681afa55d5fa231cbf81506
MirYousif <49961223+MirYousif@users.noreply.github.com>
2021-10-11 09:14:55 +0200

    Update Dingus.java

--- Dingus.java
+++ Dingus.java
@@ -26,8 +26,9 @@ abstract class Dingus {
     /**
      * initializes color and position to random values
      *
-     * @param maxX, maxY - give maximum values for the position
+     *  @param maxX, maxY - give maximum values for the position
      */    
+
      public Dingus(int maxX, int maxY) {
         this.maxX = maxX; this.maxY = maxY;
         // initialize to a random position
@@ -35,7 +36,19 @@ abstract class Dingus {
         y = random.nextInt(maxY);
         // initialize to a random color
         // TODO
+
+        int upperBound = 256;
+        Random colorIndex = new Random(); 
+        // gets a random number between 0 and 255. We use this to generate a random RGB value
+        int red = colorIndex.nextInt(upperBound);
+        int green = colorIndex.nextInt(upperBound);
+        int blue = colorIndex.nextInt(upperBound);
+        // creating a random color that will be used to fill the shape.
+        Color color = new Color(red,green,blue);
+
     }
 
-    abstract void draw(Graphics g);
+    //abstract method draw that should draw the shape on the Graphics object passed as a parameter
+    abstract void draw(Graphics g); 
+
 }
